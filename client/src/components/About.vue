3a7b05f00b94b1134cd39479bfc23d2de8ad0eee
nanawel <nanawel@gmail.com>
2021-05-17 11:14:45 +0200

    Update About page

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="about">
+    <img src="@/assets/logo.svg" width="120" height="120" alt="OSL logo">
     <h1>Our Shopping List</h1>
     <em>{{ appVersion }}-{{ appBuildId }}</em>
     <p>Copyright &copy; 2021 Anaël Ollier</p>
 
+    <hr/>
+
+    <h2>Source / Issues</h2>
+    <p><a href="https://github.com/nanawel/our-shopping-list">https://github.com/nanawel/our-shopping-list</a></p>
+    
+    <h2>License</h2>
+    <p>AGPL-3.0</p>
     <!--
     <h2>Dependencies</h2>
     <div class="dependencies">
@@ -44,5 +52,10 @@ export default {
 
 .dependencies {
   text-align: left;
+}
+
+hr {
+  max-width: 50%;
+  margin: 1em auto;
 }
 </style>
