770de0d5243776dc153e2a77ece65d93e9f423cc
nanawel <nanawel@gmail.com>
2022-04-20 13:56:31 +0200

    Fix WS:hello calls handling from v1 clients

--- server/src/ws.js
+++ server/src/ws.js
@@ -5,6 +5,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 const sha1 = require('sha1');
 
 const {app, io} = require('./app');
+const {type} = require("nanobus");
 
 io.on('connection', (socket) => {
   console.log(`Client connected: ${socket.id}`);
@@ -18,17 +19,26 @@ io.on('connection', (socket) => {
   });
 
   socket.on('hello', (data, callback) => {
-    console.log(`New connection from client ${socket.id}`, data)
+    console.log(`New connection from client ${socket.id}`, data);
+
+    // Fallback for old calls from v1 where there was no data argument specified
+    if (typeof callback !== 'function' && typeof data === 'function') {
+      callback = data;
+    }
+
     const serverVersion = {
       version: SERVER_VERSION,
       buildId: SERVER_BUILD_ID
     };
     const serverHash = sha1(JSON.stringify(serverVersion + require('./config')));
-    callback({
-      serverString: `OSL Server (${SERVER_VERSION}-${SERVER_BUILD_ID})`,
-      serverVersion,
-      serverHash
-    });
+
+    if (typeof callback === 'function') {
+      callback({
+        serverString: `OSL Server (${SERVER_VERSION}-${SERVER_BUILD_ID})`,
+        serverVersion,
+        serverHash
+      });
+    }
   });
 
   socket.on('disconnect', () => {
