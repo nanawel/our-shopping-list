088a2351ead79d63ae7872fd012992606af9f369
nanawel <nanawel@gmail.com>
2022-04-21 18:05:29 +0200

    Build optimization (?)

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify/lib'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
