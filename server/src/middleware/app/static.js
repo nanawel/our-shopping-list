192840bf0eba4fe21397dd7fcb2ac158e7a81421
nanawel <nanawel@gmail.com>
2022-05-23 19:35:58 +0200

    #38 Use agressive cache policy for assets

--- server/src/middleware/app/static.js
+++ server/src/middleware/app/static.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const {VUE_APP_DISABLE_AGRESSIVE_ASSETS_CACHING} = require('../../config');
 
 module.exports = function (app) {
-  /** @see https://expressjs.com/en/4x/api.html#express.static */
-  const opts = {
-    maxAge: '30d',
-    immutable: true
+  let opts = {};
+
+  if (process.env.NODE_ENV === 'production'
+    && !VUE_APP_DISABLE_AGRESSIVE_ASSETS_CACHING
+  ) {
+    /** @see https://expressjs.com/en/4x/api.html#express.static */
+    opts = {
+      maxAge: '30d',
+      immutable: true
+    };
   }
 
   app.use(express.static('client/dist', opts));
