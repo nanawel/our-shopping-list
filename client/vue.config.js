18e0b7b9df701227c648dfcfeff7ae24475860ab
nanawel <nanawel@gmail.com>
2022-05-23 19:48:19 +0200

    #37 Fix manifest.json icon paths

--- client/vue.config.js
+++ client/vue.config.js
@@ -32,6 +32,14 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     workboxPluginMode: 'GenerateSW',
-    workboxOptions: {}
+    workboxOptions: {},
+    iconPaths: {
+      faviconSVG: null,
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-144x144.png'
+    }
   }
 }
