10fff93a99cfdc8b5cbae510e80e5129908b0ebd
nanawel <nanawel@gmail.com>
2022-07-26 19:02:56 +0200

    Remove autoreload on SW update (causes infinite reload loops)

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
-      // Force refresh
-      setTimeout(() => {
-        window.location.reload(true)
-      }, 1000)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
