87043d6289186b0ac76acbfe022b4b30358a1614
nanawel <nanawel@gmail.com>
2024-04-14 09:44:10 +0200

    Revert back to default socket.io configuration for transport

--- client/src/service/socket-io.js
+++ client/src/service/socket-io.js
@@ -6,12 +6,15 @@ import {logger} from '@/service/logger'
 
 const sock = io({
   path: path.normalize(`/${config.BASE_URL}socket.io/`),  // Custom web root support (#58/GITHUB#18)
-  transports: ['websocket', 'polling']
+
+  // https://socket.io/docs/v4/client-options/#transports
+  //transports: ['polling', 'websocket', 'webtransport'], // Keep default
+  // https://socket.io/docs/v4/client-options/#upgrade
+  //upgrade: true
 })
 
 sock.on('connect_error', (err) => {
   logger.error('[socket-io] Connection error:', err)
-  sock.io.opts.transports = ['polling', 'websocket']
 });
 
 export {
